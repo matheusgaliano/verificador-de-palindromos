890083bc3300c30e57b53b6850ebca4e4f1c16a3
matheusgaliano <galiano.matheus@outlook.com>
2026-04-30 20:45:11 -0300

    feat: adicição de um laço na pergunta do usuário

--- palindromo.py
+++ palindromo.py
@@ -7,12 +7,17 @@ def verifica_palindromo(palavra):
     palavra_invertida = palavra_limpa[::-1]
     return palavra_limpa == palavra_invertida #retirada do if e else desnecessários
        
+while True:
+    texto_usuario = input("\nDigite uma palavra (ou 'sair' para encerrar): ")
+    
+    if texto_usuario.lower() == "sair":
+        print("Encerrando o programa... Até mais!")
+        break
 
-texto_usuario = input("Digite uma palavra: ")
 
-resultado = verifica_palindromo(texto_usuario)
+    resultado = verifica_palindromo(texto_usuario)
 
-if resultado:
-    print("A palavra que você digitou é um Palíndromo!")
-else:
-    print("A palavra que você digitou não é um Palíndromo!")
+    if resultado:
+        print("A palavra que você digitou é um Palíndromo!")
+    else:
+        print("A palavra que você digitou não é um Palíndromo!")
