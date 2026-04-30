c1fdc40bdb4ed86dbf267d5269b824baa2c13bb4
matheusgaliano <galiano.matheus@outlook.com>
2026-04-29 23:51:02 -0300

    refactor: removi condições desnecessárias

--- palindromo.py
+++ palindromo.py
@@ -1,10 +1,8 @@
 def verifica_palindromo(palavra):
     palavra = palavra.lower()
     palavra_invertida = palavra[::-1]
-    if palavra == palavra_invertida:
-        return True
-    else:
-        return False
+    return palavra == palavra_invertida
+       
 
 texto_usuario = input("Digite uma palavra: ")
 
